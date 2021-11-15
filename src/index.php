11f5b55b059ff1e87b95721d93edd878cc7e9db2
saqib_khan_tgm <saqib.khan@topgearmedia.co.uk>
2021-11-15 17:19:36 +0500

    hello world change to hello universe

--- src/index.php
+++ src/index.php
@@ -4,7 +4,7 @@ namespace saqibak01\HelloComposer;
 
 class Index
 {
-    public function greet($greet = "Hello Universe 123")
+    public function greet($greet = "Hello Universe!")
     {
         return $greet;
     }
